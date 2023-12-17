66d12cfcd8d52a3b98e9ba3bd00ae5bf2fcd2cf3
Jaden Leonard <91337033+jadenl1@users.noreply.github.com>
2023-12-16 21:13:36 -0500

    Ayeee

--- server.js
+++ server.js
@@ -66,55 +66,47 @@ let cachedMovie = null;
 
 app.get('/GetRandomMovie', async (req, res) => {
     // Check if the movie is already cached for the day
-    if (cachedMovie) {
-      res.json(cachedMovie);
-    } else {
-        const options = {
-            method: 'GET',
-            url: 'https://unogsng.p.rapidapi.com/search',
-            params: {
-              start_year: '1972',
-              orderby: 'rating',
-              audiosubtitle_andor: 'and',
-              limit: '100',
-              subtitle: 'english',
-              countrylist: '78,46',
-              audio: 'english',
-              country_andorunique: 'unique',
-              offset: '0',
-              end_year: '2019'
-            },
-            headers: {
-              'X-RapidAPI-Key': '92e3539755mshd891009343a85bfp19f985jsnc0487541d931',
-              'X-RapidAPI-Host': 'unogsng.p.rapidapi.com'
-            }
-        };    
-  
-        try {
-            const response = await axios.request(options);
+    const options = {
+        method: 'GET',
+        url: 'https://unogsng.p.rapidapi.com/search',
+        params: {
+            start_year: '1972',
+            orderby: 'rating',
+            audiosubtitle_andor: 'and',
+            limit: '100',
+            subtitle: 'english',
+            countrylist: '78,46',
+            audio: 'english',
+            country_andorunique: 'unique',
+            offset: '0',
+            end_year: '2019'
+        },
+        headers: {
+            'X-RapidAPI-Key': '92e3539755mshd891009343a85bfp19f985jsnc0487541d931',
+            'X-RapidAPI-Host': 'unogsng.p.rapidapi.com'
+        }
+    };    
 
-            const movies = response.data.results;
+    try {
+        const response = await axios.request(options);
 
-            // console.log(movies);
-    
-            if (movies.length > 0) {
+        const movies = response.data.results;
+
+        // console.log(movies);
+
+        if (movies.length > 0) {
             const randomIndex = Math.floor(Math.random() * movies.length);
             const randomMovie = movies[randomIndex];
-    
-            // Cache the random movie for the day
-            cachedMovie = randomMovie;
-            cache.set('randomMovie', cachedMovie, 86400);
 
             console.log(randomMovie);
 
             res.json(randomMovie);
-            } else {
-            res.status(404).json({ error: 'No movies found in the response' });
-            }
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+        res.status(404).json({ error: 'No movies found in the response' });
         }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
   });
 
